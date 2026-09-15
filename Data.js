dc123760504abb8d7506e2f71524143327f91aab
turtlesarefriends <briananthonypontious@gmail.com>
2026-09-14 21:05:25 -0400

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,30 @@
+const ONEMAIN_DATA = [
+  {
+    name: "Mike Azinger",
+    state: "WV",
+    level: "upper",
+    chamber: "State Senate",
+    district: "3",
+    party: "Republican",
+    branches: [
+      [
+        "Parkersburg Branch",
+        "2837 Pike St, Ste 2B, Parkersburg, WV 26101"
+      ]
+    ]
+  },
+  {
+    name: "Trenton Barnhart",
+    state: "WV",
+    level: "upper",
+    chamber: "State Senate",
+    district: "3",
+    party: "Republican",
+    branches: [
+      [
+        "Parkersburg Branch",
+        "2837 Pike St, Ste 2B, Parkersburg, WV 26101"
+      ]
+    ]
+  }
+];
